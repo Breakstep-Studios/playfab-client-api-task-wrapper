ceada2abf3aed0cdf74e2cf97c3f1165e7058ecb
Thomas <thsbrown1@gmail.com>
2022-04-22 14:31:28 -0700

    PlayFabCloudScriptResponse<T> now inherits from PlayFabCloudScriptResponse
    - All code pertaining to both classes such as LogsErrorCode has been moved in to PlayFabCloudScriptResponse.
    - ContainsError has been removed from PlayFabCloudScriptResponse<T> as it is now defined in PlayFabCloudScriptResponse.

--- Runtime/Models/PlayFabCloudScriptResponse.cs
+++ Runtime/Models/PlayFabCloudScriptResponse.cs
@@ -18,25 +18,7 @@ namespace ThomasBrown.PlayFab
         {
             get { return Result.Error != null || base.ContainsError; }
         }
-    }
-    
-    public class PlayFabCloudScriptResponse<T> : PlayFabCommonResponse<ExecuteCloudScriptResult>
-    {
-        private T functionResult;
         
-        public PlayFabCloudScriptResponse(ExecuteCloudScriptResult result, PlayFabError error) : base(result, error)
-        {
-            functionResult = (T)result?.FunctionResult;
-        }
-
-        /// <summary>
-        /// Checks if we had an error in our response, or if we had a script execution error
-        /// </summary>
-        public override bool ContainsError
-        {
-            get { return Result?.Error != null || base.ContainsError; }
-        }
-
         /// <summary>
         /// The error code contained in our logs if any <see cref="PlayFabCloudScriptResponseLogsErrorCode"/> code example for more info.
         /// </summary>
@@ -76,6 +58,16 @@ namespace ThomasBrown.PlayFab
 
                 return PlayFabCloudScriptResponseLogsErrorCode.Unknown; 
             }
+        }
+    }
+    
+    public class PlayFabCloudScriptResponse<T> : PlayFabCloudScriptResponse
+    {
+        private T functionResult;
+        
+        public PlayFabCloudScriptResponse(ExecuteCloudScriptResult result, PlayFabError error) : base(result, error)
+        {
+            functionResult = (T)result?.FunctionResult;
         }
 
         public T FunctionResult
